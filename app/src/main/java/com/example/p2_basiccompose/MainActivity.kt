f4e111e8f798fda9802cf61de127eb00584ea614
DhimasAdityaP <dimasadit0105@gmail.com>
2024-10-17 16:26:31 +0700

    Commit 7 mengatur tata letak nya dan juga ukuran layar to fill max size

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -45,6 +45,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicLayout( modifier: Modifier = Modifier) {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "Login",
             style = TextStyle(fontSize = 50.sp,
