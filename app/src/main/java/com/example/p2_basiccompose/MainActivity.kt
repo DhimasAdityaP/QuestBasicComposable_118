6f5728745f13c0748b08dc8f53421d330efa4063
DhimasAdityaP <dimasadit0105@gmail.com>
2024-10-17 17:09:47 +0700

    Commit 13 menambahkan text ketiga yaitu "20200140118"

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -69,6 +69,9 @@ fun BasicLayout( modifier: Modifier = Modifier) {
             text="Dhimas Aditya Pratama",
             style = TextStyle(fontSize = 18.sp,color= Color.Red)
         )
+        Text(
+            text="20200140118"
+        )
     }
 }
 
